39793af6b6d24e1f65dbb6623c1618f0df2e3be0
Samuel Igbinomwanhia <samuel.igb@outlook.com>
2025-12-22 14:07:39 +0100

    Added structure

---
+++ src/config/whatsapp.ts
@@ -0,0 +1,5 @@
+export const whatsappConfig = {
+  graphApiBaseUrl: "https://graph.facebook.com/v20.0",
+  accessToken: process.env.META_ACCESS_TOKEN!,
+  phoneNumberId: process.env.META_PHONE_NUMBER_ID!,
+};
